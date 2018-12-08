ff423694dc90828b588d98e31edb38d6cd77268f
William Dale <williambdale@gmail.com>
2018-12-08 16:42:30 -0500

    Create blinking external LED

--- testFiles/strobe.js
+++ testFiles/strobe.js
@@ -1,10 +1,19 @@
-let five = require("johnny-five"),
-let board = new five.Board();
+const { Led, Board } = require('johnny-five');
+const board = new Board();
+board.on('ready', onReady);
 
-board.on("ready", function() {
-  // Create an Led on pin 13
-  let led = new five.Led(13);
+function onReady() {
+  // if we don't pass a port to the Led constructor it will use
+  // the default port (built-in LED)
+  const led = new Led(22);
 
-  // Strobe the pin on/off, defaults to 100ms phases
-  led.strobe();
-});
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  board.repl.inject({
+      led: led
+  });
+
+  led.blink();
+  // run in the REPL led.stop() to make it stop blinking
+}
